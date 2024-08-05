3e6c2dbb39000d090dbd798682c43419d14d7209
ahmed hassan <a7med.abdo6@gmail.com>
2024-08-05 22:52:59 +0300

    first commit

--- src/Api/axiosConfig.js
+++ src/Api/axiosConfig.js
@@ -5,8 +5,8 @@ const axiosInstance = axios.create({
   baseURL: "http://3.72.63.179:8000/api", // Example base URL
   headers: {
     "Content-Type": "application/json",
-    jwtAuth:
-      "bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzY2MDgxNzMxLCJpYXQiOjE3MjI4ODE3MzEsImp0aSI6IjdiZWQ1NmVjMDgwOTQ4YTVhNGE3Mjk0YzdlY2RiZjZjIiwidXNlcl9pZCI6MSwibmFtZSI6IiIsInVzZXJuYW1lIjoiZWhhcHNhbXkifQ.G5yTMMhfdssAo5L4ACnQzDXTEK25UCIjlHdkDpiBnN4",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzY2MDgxNzMxLCJpYXQiOjE3MjI4ODE3MzEsImp0aSI6IjdiZWQ1NmVjMDgwOTQ4YTVhNGE3Mjk0YzdlY2RiZjZjIiwidXNlcl9pZCI6MSwibmFtZSI6IiIsInVzZXJuYW1lIjoiZWhhcHNhbXkifQ.G5yTMMhfdssAo5L4ACnQzDXTEK25UCIjlHdkDpiBnN4",
   },
 });
 
